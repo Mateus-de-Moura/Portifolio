1c637a6d0464c018f5cb0b579e895b962abb4421
Mateus-de-Moura <mateus.demoura@hotmail.com>
2022-09-27 15:12:28 -0300

    first commit

---
+++ assets/js/acordeon.js
@@ -0,0 +1,14 @@
+const acordeonElement = document.querySelectorAll(".acordeon .trigger")
+
+acordeonElement.forEach((trigger) => 
+      trigger.addEventListener('click', (e) =>{
+           const acordeon = trigger.parentElement
+           const isOpen = acordeon.classList.contains('open')
+
+           if(isOpen){
+            acordeon.classList.remove('open')
+           }else{
+            acordeon.classList.add('open')
+           }
+      })
+)
